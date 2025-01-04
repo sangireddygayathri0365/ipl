6fa18328707f76e0670b0cdc9c19b66d11cc0392
username <wecp-dummy@gmail.com>
2025-01-04 13:14:58 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/repository/MatchRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/MatchRepository.java
@@ -1,5 +1,9 @@
 package com.wecp.progressive.repository;
 
+import com.wecp.progressive.entity.Match;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface MatchRepository {
+@Repository
+public interface MatchRepository extends JpaRepository<Match, Integer> {
 }
--- backend/src/main/java/com/wecp/progressive/service/impl/MatchServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/MatchServiceImplJpa.java
@@ -1,5 +1,51 @@
 package com.wecp.progressive.service.impl;
 
-public class MatchServiceImplJpa  {
+import com.wecp.progressive.entity.Match;
+import com.wecp.progressive.repository.MatchRepository;
+import com.wecp.progressive.service.MatchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
+import java.util.List;
+
+@Service
+public class MatchServiceImplJpa implements MatchService {
+
+    private MatchRepository matchRepository;
+
+    @Autowired
+    public MatchServiceImplJpa(MatchRepository matchRepository) {
+        this.matchRepository = matchRepository;
+    }
+
+    @Override
+    public List<Match> getAllMatches() throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public Match getMatchById(int matchId) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Integer addMatch(Match match) throws SQLException {
+        return -1;
+    }
+
+    @Override
+    public void updateMatch(Match match) throws SQLException {
+
+    }
+
+    @Override
+    public void deleteMatch(int matchId) throws SQLException {
+
+    }
+
+    @Override
+    public List<Match> getAllMatchesByStatus(String status) {
+        return null;
+    }
 }
