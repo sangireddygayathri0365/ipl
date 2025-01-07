ac385eaba690adcebd4d90043f8348086a85f254
username <wecp-dummy@gmail.com>
2025-01-07 09:15:04 +0000

    wecp initial commit

--- backend/src/main/java/com/wecp/progressive/repository/TeamRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/TeamRepository.java
@@ -1,11 +1,12 @@
 package com.wecp.progressive.repository;
-
+ 
 import com.wecp.progressive.entity.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+ 
 @Repository
 public interface TeamRepository extends JpaRepository<Team, Integer> {
-
+ 
     Team findByTeamId(int teamId);
+    Team findByTeamName(String teamName);
 }
--- backend/src/main/java/com/wecp/progressive/service/impl/TeamServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/TeamServiceImplJpa.java
@@ -1,5 +1,5 @@
 package com.wecp.progressive.service.impl;
-
+ 
 import com.wecp.progressive.entity.Team;
 import com.wecp.progressive.exception.TeamAlreadyExistsException;
 import com.wecp.progressive.exception.TeamDoesNotExistException;
@@ -9,55 +9,63 @@ import com.wecp.progressive.repository.TeamRepository;
 import com.wecp.progressive.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+ 
 import java.sql.SQLException;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-
+ 
 @Service
 public class TeamServiceImplJpa  implements TeamService {
-
+ 
     private TeamRepository teamRepository;
-
+ 
     @Autowired
     CricketerRepository cricketerRepository;
-
+ 
     @Autowired
     MatchRepository matchRepository;
-
+ 
     @Autowired
     public TeamServiceImplJpa(TeamRepository teamRepository) {
         this.teamRepository = teamRepository;
     }
-
+ 
     @Override
     public List<Team> getAllTeams() throws SQLException {
         return teamRepository.findAll();
     }
-
+ 
     @Override
     public int addTeam(Team team) throws SQLException {
+       if(teamRepository.existsById(team.getTeamId())){
+        throw new TeamAlreadyExistsException("Team already exists.");
+       }
         return teamRepository.save(team).getTeamId();
     }
-
+ 
     @Override
     public List<Team> getAllTeamsSortedByName() throws SQLException {
         List<Team> sortedTeam = teamRepository.findAll();
         sortedTeam.sort(Comparator.comparing(Team::getTeamName));
         return sortedTeam;
     }
-
+ 
     @Override
     public Team getTeamById(int teamId) throws SQLException {
-        return teamRepository.findByTeamId(teamId);
+        return teamRepository.findById(teamId).orElseThrow(()-> new TeamDoesNotExistException("Team doenot exists"));
     }
-
+ 
     @Override
     public void updateTeam(Team team) throws SQLException {
-        teamRepository.save(team);
+     Team tm=  teamRepository.findByTeamName(team.getTeamName());
+     if(tm != null)
+     {
+        throw new TeamAlreadyExistsException("Team already exists.");
+     }  
+     teamRepository.save(team);
     }
-
+ 
     @Override
     public void deleteTeam(int teamId) throws SQLException {
         matchRepository.deleteByTeamId(teamId);
